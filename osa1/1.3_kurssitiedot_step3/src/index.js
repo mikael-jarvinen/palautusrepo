27b31d88cfdc5ffd7076d44c7b33391b1aba9b26
usermine12 <mikaeljarv@gmail.com>
2020-03-21 11:43:53 +0200

    osa1 kurssitiedot step3

---
+++ osa1/1.3_kurssitiedot_step3/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+  <>
+  <h1>{props.course}</h1>
+  </>
+  )
+}
+
+const Content = (props) => {
+  return (
+  <>
+    <Part part={props.part1}></Part>
+    <Part part={props.part2}></Part>
+    <Part part={props.part3}></Part>
+  </>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <>
+    <p>Number of exercises {props.total}</p>
+  </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1}
+      part2={part2}
+      part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
