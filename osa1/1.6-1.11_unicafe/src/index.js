a859a896ce5afa1a1e21cf2690a1f7f589b1e141
usermine12 <mikaeljarv@gmail.com>
2020-03-22 12:45:36 +0200

    osa1 1.6

---
+++ osa1/1.6-1.11_unicafe/src/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({text}) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Button = ({text, onClick}) => {
+  return (
+    <>
+      <button onClick={onClick}>{text}</button>
+    </>
+  )
+}
+
+const FeedbackButtons = ({badclick, neutralclick, goodclick}) => {
+  return (
+    <div>
+      <Button text='good' onClick={goodclick} />
+      <Button text='neutral' onClick={neutralclick} />
+      <Button text='bad' onClick={badclick} />
+    </div>
+  )
+}
+
+const FeedbackDisplay = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>good {good} <br></br>neutral {neutral} <br></br>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => {
+    return (
+      setGood(good + 1)
+    )
+  }
+
+  const neutralClick = () => {
+    return (
+      setNeutral(neutral + 1)
+    )
+  }
+
+  const badClick = () => {
+    return (
+      setBad(bad + 1)
+    )
+  }
+
+  return (
+    <div>
+      <Header text='give feedback' />
+      <FeedbackButtons badclick={badClick} 
+      neutralclick={neutralClick} 
+      goodclick={goodClick} 
+      />
+      <Header text='statistics' />
+      <FeedbackDisplay good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
