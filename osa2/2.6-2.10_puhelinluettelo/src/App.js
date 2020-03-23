c7e9265ea5d763c98f23d97e28cc57da5c0191b5
usermine12 <mikaeljarv@gmail.com>
2020-03-23 19:45:02 +0200

    osa2 2.6-2.10

--- osa2/2.6-2.10_puhelinluettelo/src/App.js
+++ osa2/2.6-2.10_puhelinluettelo/src/App.js
@@ -9,14 +9,15 @@ const App = () => {
         { name: 'Ada Lovelace', number: '39-44-5323523', filter: false},
         { name: 'Dan Abramov', number: '12-43-234345', filter: false },
         { name: 'Mary Poppendieck', number: '39-23-6423122', filter: false }
-      ])
+    ])
+    const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber] = useState('')
 
     const addPerson = (person, newNumber) => {
         const newNode = {name: person, number: newNumber, filter: false}
 
         setPersons(persons.concat(newNode))
     }
-
     const onSearch = (event) => {
         const search = event.target.value.toLowerCase()
 
@@ -33,6 +34,27 @@ const App = () => {
         }))
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        
+        if (persons.reduce((cond, x) => {
+            if (x.name === newName){
+                cond = true
+            }
+            return cond
+        }, false)){
+            window.alert(`${newName} is already added to phonebook`)
+        } else {
+            setNewName('')
+            setNewNumber('')
+            addPerson(newName, newNumber)
+            console.log(`added ${newName} to contacts`)
+        }
+    }
+
+    const handleNameChange = (event) => setNewName(event.target.value)
+    const handleNumberChange = (event) => setNewNumber(event.target.value)
+
     return (
     <div>
         <h2>Phonebook</h2>
@@ -40,6 +62,9 @@ const App = () => {
         <h2>add a new</h2>
         <NewName addPerson={addPerson}
         persons={persons}
+        handleNumberChange={handleNumberChange}
+        handleNameChange={handleNameChange}
+        handleSubmit={handleSubmit}
         />
         <h2>Numbers</h2>
         <Contacts persons={persons} />
--- osa2/2.6-2.10_puhelinluettelo/src/components/NewName.js
+++ osa2/2.6-2.10_puhelinluettelo/src/components/NewName.js
@@ -1,34 +1,10 @@
 import React, { useState } from 'react'
 
-const NewName = ({addPerson, persons}) => {
-    const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber] = useState('')
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        
-        if (persons.reduce((cond, x) => {
-            if (x.name === newName){
-                cond = true
-            }
-            return cond
-        }, false)){
-            window.alert(`${newName} is already added to phonebook`)
-        } else {
-            setNewName('')
-            setNewNumber('')
-            addPerson(newName, newNumber)
-            console.log(`added ${newName} to contacts`)
-        }
-    }
-
-    const handleNameChange = (event) => setNewName(event.target.value)
-    const handleNumberChange = (event) => setNewNumber(event.target.value)
-
+const NewName = ({newName, newNumber, handleNumberChange, handleNameChange, handleSubmit}) => {
     return (
         <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+            name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
             number: <input value={newNumber} onChange={handleNumberChange}/>
