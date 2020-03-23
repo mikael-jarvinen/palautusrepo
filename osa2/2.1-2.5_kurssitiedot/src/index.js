205e6c164c9f6286d7f2bbc4869b941cb3e5c1c1
usermine12 <mikaeljarv@gmail.com>
2020-03-23 11:47:50 +0200

    osa2 2.2-2.3

--- osa2/2.1-2.5_kurssitiedot/src/index.js
+++ osa2/2.1-2.5_kurssitiedot/src/index.js
@@ -11,9 +11,14 @@ const Header = (props) => {
 
 const Content = ({parts}) =>{
   return (
+    <>
     <ul>
       {parts.map((x) => <li key={x.id}>{x.name} {x.exercises}</li>)}
     </ul>
+    <b>
+      total of {parts.reduce((sum, x) => x.exercises + sum ,0)} exercises
+    </b>
+    </>
   )
 }
 
