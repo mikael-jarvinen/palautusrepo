4f6ce1bd32bded19fcb80718d0ffb06fc7a8d38d
usermine12 <mikaeljarv@gmail.com>
2020-04-16 16:17:09 +0300

    osa8 8.20

--- osa8/8.17-8.22_kirjailijat_frontend/src/App.js
+++ osa8/8.17-8.22_kirjailijat_frontend/src/App.js
@@ -5,6 +5,7 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import { useApolloClient } from '@apollo/client'
+import Recommendations from './components/Recommendations'
 
 const App = () => {
   const [page, setPage] = useState('authors')
@@ -35,6 +36,7 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage('recommendations')}>recommend</button>
         <button onClick={logout}>logout</button>
       </div>
 
@@ -45,6 +47,9 @@ const App = () => {
       <Books
         show={page === 'books'}
       />
+
+      <Recommendations
+        show={page === 'recommendations'}/>
 
       <NewBook
         show={page === 'add'}
